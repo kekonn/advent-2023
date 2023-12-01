5bb8a81f3b42e7e8bca98c5dfd01ca6a9abcb893
kekkon <149670+kekonn@users.noreply.github.com>
2023-12-01 14:00:27 +0000

    day 1 part 1 working

---
+++ src/day1.rs
@@ -0,0 +1,51 @@
+use std::collections::HashMap;
+
+use anyhow::Result;
+use tokio::io::{AsyncBufRead, AsyncRead, AsyncBufReadExt, BufReader};
+
+static  NUM_MAP: HashMap<String, u32> = HashMap::from([]);
+
+pub async fn day1(data: impl AsyncRead + Unpin) -> u32 {
+    // Convert file to buffered line by line access
+    let mut lines = to_lines(data);
+    let mut nums = Vec::<u32>::new();
+
+    while let Ok(Some(line)) = lines.next_line().await {
+        nums.push(line_to_num(&line).unwrap());
+    }
+
+    nums.iter().sum()
+}
+
+fn to_lines(data: impl AsyncRead + Unpin) -> tokio::io::Lines<impl AsyncBufRead + Unpin> {
+    let reader = BufReader::new(data);
+    reader.lines()
+}
+
+fn line_to_num(line: &str) -> Result<u32> {
+    let line_digits: Vec<char> = line.chars().filter(|c| c.is_ascii_digit()).collect();
+
+    let num_digits = format!("{}{}", line_digits.first().unwrap_or(&'0'), line_digits.last().unwrap_or(&'0'));
+
+    Ok(num_digits.parse::<u32>()?)
+}
+
+#[cfg(test)]
+mod test {
+    use tokio::io::BufReader;
+
+    const DAY1_SAMPLE_INPUT: &str = r#"1abc2
+    pqr3stu8vwx
+    a1b2c3d4e5f
+    treb7uchet"#;
+
+    /// result should be 142
+    #[tokio::test]
+    async fn day1() {
+        let str_reader = BufReader::new(DAY1_SAMPLE_INPUT.as_bytes());
+
+        let result = super::day1(str_reader).await;
+
+        assert_eq!(142, result);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
+/// https://adventofcode.com/2023/day/1
+mod day1;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    println!("Hello, world!");
+    
+    println!("---------- Day 1 ----------");
+    let day1_input = tokio::fs::File::open("data/day1.txt").await.expect("Error opening day1 input");
+    let day1_result = day1::day1(day1_input).await;
+    println!("Result: {}", day1_result);
+    println!("---------------------------");
 
     Ok(())
 }
