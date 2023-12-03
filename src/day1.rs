56334092cf4ab3b5b2313835ff4bafb65398d478
kekkon <ketsukonn@gmail.com>
2023-12-03 22:47:48 +0100

    progress on day 1 pt 2

--- src/day1.rs
+++ src/day1.rs
@@ -1,31 +1,64 @@
-use anyhow::Result;
-use tokio::io::{AsyncBufRead, AsyncRead, AsyncBufReadExt, BufReader};
+use std::collections::HashMap;
 
-static NUM_MAP: [(&str, u32); 9] = [("one", 1), ("two", 2),("three", 3),("four",4),("five",5),("six",6),("seven",7),("eight",8),("nine",9)];
+use anyhow::Result;
+use tokio::io::{ AsyncBufRead, AsyncRead, AsyncBufReadExt, BufReader };
+use regex::Regex;
+
+static NUM_MAP: [(&str, &str); 9] = [
+    ("one", "1"),
+    ("two", "2"),
+    ("three", "3"),
+    ("four", "4"),
+    ("five", "5"),
+    ("six", "6"),
+    ("seven", "7"),
+    ("eight", "8"),
+    ("nine", "9"),
+];
+const REGEX: &str = "(one|two|three|four|five|six|seven|eight|nine)";
 
 pub async fn day1(data: impl AsyncRead + Unpin) -> u32 {
     // Convert file to buffered line by line access
-    let mut lines = to_lines(data);
+    let mut lines = {
+        let reader = BufReader::new(data);
+        reader.lines()
+    };
     let mut nums = Vec::<u32>::new();
+    let regex = Regex::new(REGEX).unwrap();
+    let dictionary = HashMap::from(NUM_MAP);
 
     while let Ok(Some(line)) = lines.next_line().await {
-        nums.push(line_to_num(&line).unwrap());
+        let line = {
+            let line = line;let regex = &regex;let dictionary = &dictionary;
+            let line = line.trim().to_owned();
+            let mut line_output = line.clone();
+
+            let matches = regex.find_iter(&line);
+            for rmatch in matches {
+                let matched_str = rmatch.as_str();
+                line_output = line_output.replace(matched_str, dictionary[matched_str]);
+            }
+
+            line_output
+        };
+        nums.push({
+            let line: &str = &line;
+            let line_digits: Vec<char> = line
+                .chars()
+                .filter(|c| c.is_ascii_digit())
+                .collect();
+
+            let num_digits = format!(
+                "{}{}",
+                line_digits.first().unwrap_or(&'0'),
+                line_digits.last().unwrap_or(&'0')
+            );
+
+            Ok(num_digits.parse::<u32>()?)
+        }.unwrap());
     }
 
     nums.iter().sum()
-}
-
-fn to_lines(data: impl AsyncRead + Unpin) -> tokio::io::Lines<impl AsyncBufRead + Unpin> {
-    let reader = BufReader::new(data);
-    reader.lines()
-}
-
-fn line_to_num(line: &str) -> Result<u32> {
-    let line_digits: Vec<char> = line.chars().filter(|c| c.is_ascii_digit()).collect();
-
-    let num_digits = format!("{}{}", line_digits.first().unwrap_or(&'0'), line_digits.last().unwrap_or(&'0'));
-
-    Ok(num_digits.parse::<u32>()?)
 }
 
 #[cfg(test)]
@@ -37,6 +70,18 @@ mod test {
     a1b2c3d4e5f
     treb7uchet"#;
 
+    const DAY1_PT2_INPUT: &str =
+        r#"two1nine
+    eightwothree
+    abcone2threexyz
+    xtwone3four
+    4nineeightseven2
+    zoneight234
+    7pqrstsixteen
+    "#;
+
+    const DAY1_PT2_TEST: &str = "nineight";
+
     /// result should be 142
     #[tokio::test]
     async fn day1() {
@@ -45,5 +90,23 @@ mod test {
         let result = super::day1(str_reader).await;
 
         assert_eq!(142, result);
+    }
+
+    #[tokio::test]
+    async fn day1_pt2() {
+        let str_reader = BufReader::new(DAY1_PT2_INPUT.as_bytes());
+
+        let result = super::day1(str_reader).await;
+
+        assert_eq!(281, result);
+    }
+
+    #[tokio::test]
+    async fn day1_pt2bis() {
+        let str_reader = BufReader::new(DAY1_PT2_TEST.as_bytes());
+
+        let result = super::day1(str_reader).await;
+
+        assert_eq!(98, result);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ mod day1;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    
     println!("---------- Day 1 ----------");
-    let day1_input = tokio::fs::File::open("data/day1.txt").await.expect("Error opening day1 input");
-    let day1_result = day1::day1(day1_input).await;
-    println!("Result: {}", day1_result);
+    {
+        let pt2_input = tokio::fs::File::open("data/day1_pt2.txt").await.expect("Error opening day1 pt. 2 input");
+        println!("Result: {}", day1::day1(pt2_input).await);
+    }
     println!("---------------------------");
 
     Ok(())
