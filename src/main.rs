643de1b94840f35154ecb78eb1f1d9a2f9e5ee76
Jonas Van der Aa <jonas.vanderaa@willemen.be>
2023-12-04 16:16:33 +0100

    day1 has same input

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod day1;
 async fn main() -> anyhow::Result<()> {
     println!("---------- Day 1 ----------");
     {
-        let pt2_input = tokio::fs::File::open("data/day1_pt2.txt").await.expect("Error opening day1 pt. 2 input");
+        let pt2_input = tokio::fs::File::open("data/day1.txt").await.expect("Error opening day1 input");
         println!("Result: {}", day1::day1(pt2_input).await);
     }
     println!("---------------------------");
